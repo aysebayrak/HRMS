eb7ad822a4e3a03911de3d0f2c5b5f752ec27e2e
Ayşe BAYRAK <202803003@ogr.cbu.edu.tr>
2021-05-24 15:43:17 +0300

    .

--- github.com/aysebayrak812/HRMS.git/HRMS/src/main/java/kodlamaio/HRMS/business/concrete/CandidateManager.java
+++ github.com/aysebayrak812/HRMS.git/HRMS/src/main/java/kodlamaio/HRMS/business/concrete/CandidateManager.java
@@ -2,35 +2,53 @@ package kodlamaio.HRMS.business.concrete;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import kodlamaio.HRMS.business.abstracts.CandidateService;
 import kodlamaio.HRMS.core.utilities.result.DataResult;
+import kodlamaio.HRMS.core.utilities.result.ErrorResult;
 import kodlamaio.HRMS.core.utilities.result.Result;
+import kodlamaio.HRMS.core.utilities.result.SuccessDataResult;
+import kodlamaio.HRMS.core.utilities.result.SuccessResult;
+import kodlamaio.HRMS.dataAccess.abstracts.CandidateDao;
 import kodlamaio.HRMS.entities.concretes.Candidate;
 
 public class CandidateManager implements CandidateService {
+	
+private CandidateDao candidateDao;
+	
+	@Autowired
+	public CandidateManager(CandidateDao candidateDao) {
+		super();
+		this.candidateDao = candidateDao;
+	}
+	
 
 	@Override
 	public DataResult<List<Candidate>> getAll() {
-		// TODO Auto-generated method stub
-		return null;
+		return new SuccessDataResult<List<Candidate>>(this.candidateDao.findAll(),"DATA LİSTELENDİ");
 	}
 
 	@Override
 	public DataResult<Candidate> getByNationalIdentity(String nationalIdentity) {
-		// TODO Auto-generated method stub
-		return null;
+		return new SuccessDataResult<>(this.candidateDao.findByNationalIdentity(nationalIdentity));
 	}
 
 	@Override
 	public DataResult<Candidate> getByEmail(String email) {
-		// TODO Auto-generated method stub
-		return null;
+	       return new SuccessDataResult<>(this.candidateDao.findByEmail(email));
 	}
 
 	@Override
 	public Result add(Candidate candidate) {
-		// TODO Auto-generated method stub
-		return null;
+		if(getByNationalIdentity(candidate.getNationalIdentity()).getData()!= null) {
+			return new ErrorResult("Bu kimlik zaten var ");
+		}
+		else if(getByEmail(candidate.getEmail()).getData()!=null) {
+			return new ErrorResult("BU MAİL VAR");
+		}
+		this.candidateDao.save(candidate);
+		return new SuccessResult("iş arayan eklendi");
 	}
 
 }
---
+++ github.com/aysebayrak812/HRMS.git/HRMS/src/main/java/kodlamaio/HRMS/business/concrete/EmployerManager.java
@@ -0,0 +1,43 @@
+package kodlamaio.HRMS.business.concrete;
+
+import java.util.List;
+
+import kodlamaio.HRMS.business.abstracts.EmployerService;
+import kodlamaio.HRMS.core.utilities.result.DataResult;
+import kodlamaio.HRMS.core.utilities.result.ErrorResult;
+import kodlamaio.HRMS.core.utilities.result.Result;
+import kodlamaio.HRMS.core.utilities.result.SuccessDataResult;
+import kodlamaio.HRMS.core.utilities.result.SuccessResult;
+import kodlamaio.HRMS.dataAccess.abstracts.EmployerDao;
+import kodlamaio.HRMS.entities.concretes.Employer;
+
+public class EmployerManager  implements EmployerService{
+	
+	private EmployerDao employerDao;
+	
+	public EmployerManager(EmployerDao  employerDao ) {
+		super();
+		this.employerDao=employerDao;
+	}
+
+	@Override
+	public DataResult<List<Employer>> getAll() {
+		return new SuccessDataResult<List<Employer>>(this.employerDao.findAll());
+	}
+
+	@Override
+	public DataResult<Employer> getByEmail(String email) {
+		return new SuccessDataResult<>(this.employerDao.findByEmail(email));
+		
+	}
+
+	@Override
+	public Result add(Employer employer) {
+		if(getByEmail(employer.getEmail()).getData()!=null) {
+			return new ErrorResult("email var ");
+		}
+		this.employerDao.save(employer);
+		return new SuccessResult("İŞ VEREN EKLENDİ");
+	}
+
+}
