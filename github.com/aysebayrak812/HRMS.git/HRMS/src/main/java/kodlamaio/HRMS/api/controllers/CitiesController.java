d71aebaa1cf52f13195c1106a4dd0a5523020bcc
Ayşe BAYRAK <202803003@ogr.cbu.edu.tr>
2021-05-29 09:28:35 +0300

    .

---
+++ github.com/aysebayrak812/HRMS.git/HRMS/src/main/java/kodlamaio/HRMS/api/controllers/CitiesController.java
@@ -0,0 +1,30 @@
+package kodlamaio.HRMS.api.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+import kodlamaio.HRMS.business.abstracts.CityService;
+import kodlamaio.HRMS.core.utilities.result.DataResult;
+import kodlamaio.HRMS.entities.concretes.City;
+
+@RestController
+@RequestMapping("/api/cities")
+public class CitiesController {
+	
+	private CityService cityService;
+
+	public CitiesController(CityService cityService) {
+		super();
+		this.cityService = cityService;
+	}
+	
+	@GetMapping("/getall")
+	public DataResult<List<City>> getAll(){
+		return this.cityService.getAll();
+		
+	}
+	
+	
+
+}
